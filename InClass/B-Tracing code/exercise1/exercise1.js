22d26ae8854880d0b12612b85ad117057f2bc5a6
Adriana Ceron <adrianaceron@Adreana.home>
2020-10-06 18:34:01 +0200

    EX B

--- InClass/B-Tracing code/exercise1/exercise1.js
+++ InClass/B-Tracing code/exercise1/exercise1.js
@@ -1,19 +1,20 @@
-function greetPeople(people) {
-  var greeting = "He";
+const greetPeople = people => {
+  let greeting = "He";
   greeting = greeting + "llo";
   greeting = greeting + " ";
 
-  people.forEach(function(person) {
-    greeting = greeting + person;
-	console.log(greeting);
+  people.forEach(person => {
+    //greeting = greeting + person;
+    greeting = `Hello ${person}`;
+    console.log(greeting);
   });
 
   return greeting;
-}
+};
 
 /*
   Let's trace this piece of code - what is the value of result with this input
   
   */
-  var mentors = ['Irina', 'Ashleigh', 'Etza'];
-  var result = greetPeople(mentors)
+var mentors = ['Irina', 'Ashleigh', 'Etza'];
+var result = greetPeople(mentors)
