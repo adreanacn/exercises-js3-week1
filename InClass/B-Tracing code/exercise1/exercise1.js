874b8399cc349b4f5d1baa5512282de6d7107709
Adriana Ceron <adrianaceron@Adreana.home>
2020-10-06 18:47:01 +0200

    EX1 B

--- InClass/B-Tracing code/exercise1/exercise1.js
+++ InClass/B-Tracing code/exercise1/exercise1.js
@@ -2,19 +2,25 @@ const greetPeople = people => {
   let greeting = "He";
   greeting = greeting + "llo";
   greeting = greeting + " ";
-
-  people.forEach(person => {
-    //greeting = greeting + person;
-    greeting = `Hello ${person}`;
-    console.log(greeting);
-  });
-
+  // people.forEach(person => {
+  //   greeting = `${greeting} ${person}, `;
+  //   console.log(greeting);
+  // });
+  for (let i = 0; i < people.length; i++) {
+    if (i === people.length - 2) {
+      greeting = `${greeting} ${people[i]} and `;
+    } else if (i === people.length - 1) {
+      greeting = `${greeting} ${people[i]}.`;
+    } else {
+      greeting = `${greeting} ${people[i]}, `;
+    }
+  }
+  console.log(greeting);
   return greeting;
 };
-
 /*
   Let's trace this piece of code - what is the value of result with this input
-  
   */
-var mentors = ['Irina', 'Ashleigh', 'Etza'];
-var result = greetPeople(mentors)
+// const mentors = ["Irina", "Ashleigh", "Etza"];
+const mentors = ["Irina", "Ashleigh", "Etza", "Alfonso", "Ana", "Daniela", "David"];
+const result = greetPeople(mentors);
